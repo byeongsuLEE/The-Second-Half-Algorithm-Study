b928c1fe490c09c31cc837eb362def9a9a7d7d31
zzun73 <736300@naver.com>
2024-07-18 21:24:36 +0900

    refactor: 클래스명 수정

--- zzun73/BOJ_13164.java
+++ zzun73/BOJ_13164.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ_13164 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
