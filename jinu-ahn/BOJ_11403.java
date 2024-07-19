a83dd6d94ce5e357f0f8e51bcbf1d93578e79b69
jinu-ahn <66781422+jinu-ahn@users.noreply.github.com>
2024-07-19 23:10:49 +0900

    경로 찾기 / 30분

---
+++ jinu-ahn/BOJ_11403.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11403 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] floyd = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                floyd[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(floyd[i][k] == 1 && floyd[k][j] == 1) floyd[i][j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(floyd[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
